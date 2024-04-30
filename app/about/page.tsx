320c2afc2a63620f3fa65c3b5a4bed13db43c36b
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-04-30 11:04:17 -0300

    adjustments to the layout and add database seed

--- app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-const Teste = () => {
-  return (
-    <div className="bg-red-500 p-5">
-      <h1 className="text-3xl font-bold underline">About Page</h1>
-    </div>
-  )
-}
-
-export default Teste
