abc40866aac9c4d665ce7a5ce10907cb3a2811cb
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-05-02 13:58:58 -0300

    feat: add cart and cart-item

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 import './globals.css'
+import { CartProvider } from './_context/cart'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,7 +21,9 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/icon.png" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <CartProvider>{children}</CartProvider>
+      </body>
     </html>
   )
 }
