8546118647b45e130f10ce28d0b526a51fbedf08
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-05-04 17:25:44 -0300

    feat: ensure restaurant can be favorited

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
       <body className={inter.className}>
         <AuthProvider>
           <CartProvider>{children}</CartProvider>
-          <Toaster richColors />
         </AuthProvider>
+        <Toaster richColors />
       </body>
     </html>
   )
